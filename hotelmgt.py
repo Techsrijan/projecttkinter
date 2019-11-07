ea4d722ea0e1b85b634b936b7c76cf07b3bd903f
Ashwani <aswanibtech@gmail.com>
2019-11-07 14:39:54 +0530

    project

--- hotelmgt.py
+++ hotelmgt.py
@@ -1,9 +1,39 @@
 from tkinter import *
+import pymysql
+
+
+############### database conncetion #########################
+def dbconfig():
+    global mycursor,conn
+    conn = pymysql.connect(host="localhost", user="root", db="wahtaz")
+    mycursor = conn.cursor()
 
 ############# create login window ######################
 
 def loginwindow():
-    mainheading()
+    usernameVar.set("")
+    passwordVar.set("")
+    loginLabel = Label(taz, text="Admin Login", font="Arial 30")
+    loginLabel.grid(row=1, column=2, padx=(50, 0), columnspan=2, pady=10)
+
+    usernameLabel = Label(taz, text="Username")
+    usernameLabel.grid(row=2, column=2, padx=20, pady=5)
+
+    passwordLabel = Label(taz, text="Password")
+    passwordLabel.grid(row=3, column=2, padx=20, pady=5)
+
+    usernameEntry = Entry(taz, textvariable=usernameVar)
+    usernameEntry.grid(row=2, column=3, padx=20, pady=5)
+
+
+    passwordEntry = Entry(taz, show="*", textvariable=passwordVar)
+    passwordEntry.grid(row=3, column=3, padx=20, pady=5)
+
+    loginButton = Button(taz, text="Login", width=20, height=2, fg="green", bd=10, command=adminlogin)
+    loginButton.grid(row=4, column=2, columnspan=2)
+
+
+
 
 
 #############################################################
